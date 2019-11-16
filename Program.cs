c6de70a760d7733020c17914b6ec7fae529dfa29
TristanCho <51831659+TristanCho@users.noreply.github.com>
2019-11-16 22:49:13 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realizarTarea();
+        }
+        static void realizarTarea()
+        {
+            Punto origen = new Punto();
+            Punto destino = new Punto(128, 80);
+        }
+    }   
+}//TODO: TEST 2
